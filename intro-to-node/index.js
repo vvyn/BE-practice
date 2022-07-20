ce25655b65e55a08d70b944229faf085d47db34f
Victoria Vynnychok <62407356+vvyn@users.noreply.github.com>
2022-07-19 23:53:39 -0500

    nodejs practice project

---
+++ intro-to-node/index.js
@@ -0,0 +1,19 @@
+//copying one's file content's into another
+/**
+const fs = require("fs");
+fs.copyFileSync("file1.txt", "file2.txt");
+*/
+
+//using the hero/villain npm packages
+/**
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+
+var mySuperName = superheroes.random();
+var myVillainName = supervillains.random();
+
+
+console.log(myVillainName);
+ */
+
